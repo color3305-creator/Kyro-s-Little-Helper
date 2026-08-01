dc339fd12c4130b1f6235ac4cffbb34467e4da7c
color3305-creator <color3305@gmail.com>
2026-08-01 13:43:25 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -11,7 +11,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "dnd",
+    status: "Hello. I am Kyro's Utilities Bot! Nice to meet you!",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
